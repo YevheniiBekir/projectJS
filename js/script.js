3434822e388d5e10dea79a4cafd22baf12656951
YevheniiBekir <116861806+YevheniiBekir@users.noreply.github.com>
2022-12-23 02:50:50 +0200

    Replacing some elements and addition li-elements in ul-list

--- js/script.js
+++ js/script.js
@@ -23,3 +23,26 @@ const movieDB = {
         "Скотт Пілігрім проти..."
     ]
 };
+
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+adv.forEach(item => {
+item.remove();
+});
+
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+genre.textContent = 'Драма';
+
+movieList.innerHTML = '';
+movieDB.movies.sort();
+movieDB.movies.forEach((item, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i+1} ${item}
+            <div class="delete"></div>
+        </li>
+    `;
+});
