6f7b73781343cc57d0f57cae6bf3c567ff6a7876
YevheniiBekir <116861806+YevheniiBekir@users.noreply.github.com>
2022-12-23 05:31:36 +0200

    Createin of different event

--- js/script.js
+++ js/script.js
@@ -46,3 +46,59 @@ movieDB.movies.forEach((item, i) => {
         </li>
     `;
 });
+
+
+
+
+
+// Спрощення console.log();
+function log (element) {
+    return console.log(element);
+}
+
+
+
+//Створив подію, яку обробник буде створювати до тих пір, поки вона не дорівнює 3
+/*
+let i = 0;
+const clickOnFilm = document.querySelector('.promo__mnu-item');
+
+const delEl = (e) => {
+    console.log(e.target);
+    i++;
+    if(i == 3){
+        clickOnFilm.removeEventListener('click', delEl);
+    }
+};
+
+clickOnFilm.addEventListener('click', delEl);
+*/
+
+
+
+
+//Створив подію, яка відмінює стандартну поведінку браузера. 
+//Таким чином можна замість переходу по посиланню зробити щось, що мені потрібно
+/*
+const clickOnFilm = document.querySelector('.promo__mnu-item');
+
+clickOnFilm.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('Бро !');
+});
+*/
+
+
+
+//Розкрешення всіх елементів псевдомасиву
+/*
+const clickOnFilm = document.querySelectorAll('.promo__mnu-item');
+
+const paintEl = (e) => {
+    e.target.style.color = 'green';
+};
+
+clickOnFilm.forEach(item => {
+item.addEventListener('click', paintEl);
+});
+*/
