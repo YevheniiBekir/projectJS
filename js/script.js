333b5388024f964db4cd97a4dcfa9079dc69f7fb
YevheniiBekir <116861806+YevheniiBekir@users.noreply.github.com>
2022-12-25 04:54:49 +0200

    Creation exercise with ITERATION function and RECURSION function

--- js/script.js
+++ js/script.js
@@ -116,4 +116,97 @@ for(let node of document.body.childNodes){
 
 
 
+//Задача на створення змінної із загальним відсотком та студентами, які при діленні дадуть нам середній відсоток. 
+//ІТЕРАТИВНА ФУНКЦІЯ
+/*
+let students = {
+    js: [{
+        name: 'John',
+        progress: 100
+    }, {
+        name: 'Ivan',
+        progress: 60
+    }],
+    html: {
+        basic: [{
+                name: 'Peter',
+                progress: 20
+            }, {
+                name: 'Ann',
+                progress: 18
+            }],
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }]
+    },
+    ts: {
+        basic: [{
+            name: 'Pit',
+            progress: 30
+        }, {
+            name: 'Joe',
+            progress: 10
+        }],
+        pro: [{
+            name: 'Perez',
+            progress: 15
+        }, {
+            name: 'Jack',
+            progress: 16
+        }]
+    }
+};
 
+function getTotalProcentByIteration(data){
+ let total = 0;
+ let student = 0;
+
+ for(let course of Object.values(data)){
+    if(Array.isArray(course)){
+        student += course.length;
+
+        for( let i = 0; i < course.length; i++){
+            total += course[i].progress;
+        }
+    } else {
+        for (let secondCourse of Object.values(course)){
+            student += secondCourse.length;
+
+            for ( let i = 0; i < secondCourse.length; i++){
+                total += secondCourse[i].progress;
+            }
+        }
+    }
+ }
+ return total / student;
+}
+console.log(getTotalProcentByIteration(students));
+*/
+
+//РЕКУРСИВНА ФУНКЦІЯ до задачі
+/*
+function getTotalProcentByRecursion (data){
+if(Array.isArray(data)){
+    let total = 0;
+
+    for(let i = 0; i < data.length; i++){
+        total += data[i].progress;
+    }
+
+    return [total, data.length];
+} else {
+    const total = [0, 0];
+
+    for(let secondData of Object.values(data)){
+        const secondDataArr = getTotalProcentByRecursion(secondData);
+        total[0] += secondDataArr[0];
+        total[1] += secondDataArr[1];
+    }
+
+    return total;
+}
+}
+const res = getTotalProcentByRecursion(students);
+console.log(res[0]/res[1]);
+*/
